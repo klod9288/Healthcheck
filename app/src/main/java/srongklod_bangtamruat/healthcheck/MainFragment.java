00b840883ae504ada8f0f73055f5b4732b83a60a
srongklod <klod9288@gmail.com>
2019-01-18 13:23:00 +0700

    Create Register Success

--- app/src/main/java/srongklod_bangtamruat/healthcheck/MainFragment.java
+++ app/src/main/java/srongklod_bangtamruat/healthcheck/MainFragment.java
@@ -2,10 +2,12 @@ package srongklod_bangtamruat.healthcheck;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +20,30 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Content
+
+        registerContent();
+
+
+    }//Main Method
+
+    private void registerContent() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment()).addToBackStack(null).commit();
+
+            }
+        });
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
